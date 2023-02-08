463e11364b1143752c49650e0d7cfcbc08b8111a
Vini <ixxvinicius@gmail.com>
2023-02-08 16:44:37 -0300

    Finalizando o exercicio Calculo de circunferencia

--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
-﻿//Exercicio Soma
+﻿using System;
+
+
+//Exercicio Área do Círculo´
+double N = 3.14159;
+double raio = double.Parse(Console.ReadLine());
+double area = N * (raio * raio);
+Console.WriteLine($"A = {area.ToString("F0")}");
+//Exercicio Soma
+/*
 int A = int.Parse(Console.ReadLine());
 int B = int.Parse(Console.ReadLine());
 int X = A + B;
@@ -8,7 +17,7 @@ Console.WriteLine($"X = {X}");
 
 
 //Exércicio Hello World
-/*
+
 Console.WriteLine("Hello World!");
 */
 
