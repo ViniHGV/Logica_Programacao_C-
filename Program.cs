3ca23777f41fa16e6ed73a89ac2ba606dc3d88b8
Vini <ixxvinicius@gmail.com>
2023-02-08 15:43:13 -0300

    Logica com C#

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite a sua idade:");
+int idade = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Sua idade é {idade}");
+
