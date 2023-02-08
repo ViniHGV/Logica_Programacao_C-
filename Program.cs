d94baf2cc6606cfa9fa2cd62dbb7642bd3ab8cb1
Vini <ixxvinicius@gmail.com>
2023-02-08 17:01:46 -0300

    Finalizando o Exercicio Média 1

--- Program.cs
+++ Program.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 
+//Exercicio Média 1
+double N1 = double.Parse(Console.ReadLine());
+double N2 = double.Parse(Console.ReadLine());
+double Media = ((N1 * 3.5) + (N2 * 7.5)) /11;
+Media /= 10;
+
+Console.WriteLine($"MEDIA = {Math.Round(Media,5)}");
+
+
+
 //Exercicio Produto Simples
+/*
 int X = int.Parse(Console.ReadLine());
 int Y = int.Parse(Console.ReadLine());
 int Prod = X * Y;
 
 Console.WriteLine($"PROD = {Prod}");
+
 
 //Exercicio Área do Círculo
 /*
@@ -17,6 +29,7 @@ Console.WriteLine($"A = {area.ToString("F0")}");
 
 
 //Exercicio Soma
+/*
 int A = int.Parse(Console.ReadLine());
 int B = int.Parse(Console.ReadLine());
 int X = A + B;
@@ -24,7 +37,8 @@ int X = A + B;
 Console.WriteLine($"X = {X}");
 
 
-//Exércicio Hello World
+//Exércicio Hello Worl
+/*
 Console.WriteLine("Hello World!");
 */
 
