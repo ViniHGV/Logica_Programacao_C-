795f130f1ac0699b6e907735ab69faa58ee9817c
Vini <ixxvinicius@gmail.com>
2023-02-08 16:48:58 -0300

    Finalizando o exercicio Produto Simples

--- Program.cs
+++ Program.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 
+//Exercicio Produto Simples
+int X = int.Parse(Console.ReadLine());
+int Y = int.Parse(Console.ReadLine());
+int Prod = X * Y;
 
-//Exercicio Área do Círculo´
+Console.WriteLine($"PROD = {Prod}");
+
+//Exercicio Área do Círculo
+/*
 double N = 3.14159;
 double raio = double.Parse(Console.ReadLine());
 double area = N * (raio * raio);
+
 Console.WriteLine($"A = {area.ToString("F0")}");
+
+
 //Exercicio Soma
-/*
 int A = int.Parse(Console.ReadLine());
 int B = int.Parse(Console.ReadLine());
 int X = A + B;
@@ -15,9 +24,7 @@ int X = A + B;
 Console.WriteLine($"X = {X}");
 
 
-
 //Exércicio Hello World
-
 Console.WriteLine("Hello World!");
 */
 
